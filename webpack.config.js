5a8859d39ea5692eeafd1ae4273a814ccb951f62
stargazer39 <dehemiweerakkodi2@gmail.com>
2021-06-21 15:00:04 +0530

    Added asset copy plugin

--- webpack.config.js
+++ webpack.config.js
@@ -1,7 +1,16 @@
 const path = require('path');
+const CopyPlugin = require('copy-webpack-plugin');
 
 module.exports = {
     entry: './src/index.ts',
+    devtool: 'inline-source-map',
+    plugins: [
+        new CopyPlugin({
+            patterns: [
+                { from: path.join(__dirname, "src"), to: path.join(__dirname, "dist/") }
+            ]
+        })
+    ],
     module: {
         rules: [
             {
